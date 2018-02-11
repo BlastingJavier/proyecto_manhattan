9489b474f3599d7033ef915fd9b59b6f24fbcb30
BlastingJavier <31637787+BlastingJavier@users.noreply.github.com>
2018-02-11 01:50:46 +0100

    Update P1_e1.c

--- P1_e1.c
+++ P1_e1.c
@@ -1,3 +1,4 @@
+
 #include<stdio.h>
 #include<string.h>
 #include <stdlib.h>
@@ -6,19 +7,35 @@
 int main (){
   Point *p1;
   Point *p2;
-  p1 = point_ini (1,2,+);
-  p2 = point_ini (3,4,'o');
+  p1 = point_ini (1,2,BARRIER);
+  p2 = point_ini (3,4,OUTPUT);
 
   point_print (stdout,p1);
   point_print (stdout,p2);
-  if ()
+  fprintf (stdout,"\n");
 
+  if (point_equals(p1,p2)==FALSE){
+    fprintf(stdout,"Son iguales : NO");
+  }
+  else {
+    fprintf(stdout,"Son iguales : SI");
 
+  }
+  fprintf(stdout,point_getSymbol(p1));
+  fprintf(stdout,point_getCoordinateX(p2));
+  p2 = point_copy(p1);
+  fprintf (stdout,point_print(stdout,p1));
+  fprintf (stdout,point_print(stdout,p2));
 
+  if (point_equals(p1,p2)==FALSE){
+    fprintf(stdout,"Son iguales : NO");
+  }
+  else {
+    fprintf(stdout,"Son iguales : SI");
+  }
 
-
-
-
+  point_destroy(p1);
+  point_destroy(p2);
 
   return 0;
 }
