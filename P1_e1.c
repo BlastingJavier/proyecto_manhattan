669d74dd02a9f65abb15d6c40ff00b334ec5057a
BlastingJavier <31637787+BlastingJavier@users.noreply.github.com>
2018-02-15 23:43:03 +0100

    Update P1_e1.c

--- P1_e1.c
+++ P1_e1.c
@@ -7,6 +7,8 @@
 int main (){
   Point *p1;
   Point *p2;
+  char symbolaux;
+  int coordenadaaux;
   p1 = point_ini (1,2,BARRIER);
   p2 = point_ini (3,4,OUTPUT);
 
@@ -19,13 +21,21 @@ int main (){
   }
   else {
     fprintf(stdout,"Son iguales : SI");
-
   }
-  fprintf(stdout,point_getSymbol(p1));
-  fprintf(stdout,point_getCoordinateX(p2));
+  fprintf (stdout,"\n");
+
+  symbolaux = point_getSymbol(p1);
+  coordenadaaux = point_getCoordinateX(p2);
+
+  fprintf(stdout,"El simbolo del primer punto es : %c",symbolaux);
+  fprintf (stdout,"\n");
+  fprintf(stdout,"La coordenada del segundo punto es : %d",coordenadaaux);
+  fprintf (stdout,"\n");
+
   p2 = point_copy(p1);
-  fprintf (stdout,point_print(stdout,p1));
-  fprintf (stdout,point_print(stdout,p2));
+  point_print(stdout,p1);
+  point_print(stdout,p2);
+  fprintf (stdout,"\n");
 
   if (point_equals(p1,p2)==FALSE){
     fprintf(stdout,"Son iguales : NO");
@@ -33,7 +43,8 @@ int main (){
   else {
     fprintf(stdout,"Son iguales : SI");
   }
-
+  fprintf (stdout,"\n");
+  
   point_destroy(p1);
   point_destroy(p2);
 
